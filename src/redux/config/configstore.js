640e53a2e5150f80db26349d9d1d85aeed8c376f
Suebin An <jejupalette@naver.com>
2023-01-06 13:41:56 +0900

    feat : theme provider 적용

--- src/redux/config/configstore.js
+++ src/redux/config/configstore.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import darkTheme from "../modules/themeSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    darkTheme,
+  },
 });
 
 export default store;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
 import Main from "../pages/Main";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import Layout from "../layout/Layout";
+import { darkTheme, lightTheme } from "../styles/theme";
+import { useSelector } from "react-redux";
 
 const Router = () => {
+  const darkMode = useSelector((state) => state.darkTheme.darkTheme);
+  console.log(darkMode);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Main />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Main />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
