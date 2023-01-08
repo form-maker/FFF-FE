c37a7320385f24ddbf11af8623cb0c69fada2cde
Suebin An <jejupalette@naver.com>
2023-01-08 22:36:18 +0900

    feat : font, color 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,13 +3,17 @@ export const darkTheme = {
   backgroundColor: "rgb(50,50,50)",
   mainColor: "rgb(220,220,220)",
   subColor: "#A0D5FA",
+  sideColor1: "#D2E3FC",
+  sideColor2: "#EEEFF2",
+  pointColor1: "#FCE0BC",
+  pointColor2: "#DFD9FA",
 };
 
 export const lightTheme = {
   color: "#000000",
   backgroundColor: "#FFFFFF",
   mainColor: "#BBE0FA",
-  subColor: "#A0D5FA",
+  subColor: "#7BBEED",
   sideColor1: "#D2E3FC",
   sideColor2: "#EEEFF2",
   pointColor1: "#FCE0BC",
