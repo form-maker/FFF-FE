4bd5660a91976c9d258da9f987fa80e80bd504fd
Suebin An <jejupalette@naver.com>
2023-01-12 14:41:38 +0900

    feat : createForm 페이지 리덕스 기능 추가

---
+++ src/redux/modules/createFormSlice.js
@@ -0,0 +1,113 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  selectedFormType: "Cover",
+  currentPageNum: 1,
+  formList: {
+    title: "",
+    startedAt: "",
+    endedAt: "",
+    achievement: "",
+    groupList: [],
+    summary: "",
+    questionList: [],
+  },
+  error: null,
+};
+
+const createFormSlice = createSlice({
+  name: "createForm",
+  initialState,
+  reducers: {
+    changeField: (state, { payload: { form, key, value } }) => {
+      state[form][key] = value;
+    },
+    initializeForm: (state) => ({
+      state: initialState,
+    }),
+    selectedFormType(state, action) {
+      state.selectedFormType = action.payload;
+    },
+    // 형식 추가
+    addForm(state, action) {
+      state.formList.questionList = [
+        ...state.formList.questionList,
+        action.payload,
+      ];
+      state.currentPageNum = state.currentPageNum + 1;
+    },
+    // 설문 전체의 제목 작성
+    fillOutTitle(state, action) {
+      state.formList.questionList = [
+        ...state.formList.questionList,
+        action.payload,
+      ];
+    },
+    // 설문 전체의 제목 및 개요 작성
+    fillOutQuestionTitleAndSummery(state, { payload: { key, value } }) {
+      state.formList.questionList[state.currentPageNum - 2][key] = value;
+    },
+    // 질문 내용 채우기
+    fillOutQuestion(state, action) {
+      state.formList.questionList[state.currentPageNum - 2] = {
+        ...state.formList.questionList[state.currentPageNum - 2],
+        ...action.payload,
+      };
+    },
+    // 질문 삭제하기
+    deleteQuestion(state, action) {
+      const id = action.payload;
+      state.formList.questionList = state.formList.questionList.filter(
+        (question) => {
+          return question.questionId !== id;
+        }
+      );
+      state.currentPageNum = state.currentPageNum - 1;
+      state.selectedFormType =
+        state.formList.questionList[state.currentPageNum - 2]["questionType"];
+    },
+
+    // 화살표 버튼
+    goBack(state) {
+      if (state.currentPageNum > 2) {
+        state.currentPageNum = state.currentPageNum - 1;
+        state.selectedFormType =
+          state.formList.questionList[state.currentPageNum - 2]["questionType"];
+      } else {
+        state.currentPageNum = 1;
+        state.selectedFormType = "Cover";
+      }
+    },
+    goNext(state) {
+      if (
+        state.currentPageNum < state.formList.questionList?.length &&
+        state.formList.questionList.length !== 0
+      ) {
+        state.currentPageNum = state.currentPageNum + 1;
+        state.selectedFormType =
+          state.formList?.questionList[state.currentPageNum - 2][
+            "questionType"
+          ];
+      } else {
+        state.currentPageNum = state.formList.questionList.length + 1;
+        state.selectedFormType =
+          state.formList.questionList[state.formList.questionList.length - 1][
+            "questionType"
+          ];
+      }
+    },
+  },
+});
+
+export const {
+  changeField,
+  selectedFormType,
+  fillOutQuestion,
+  fillOutQuestionTitleAndSummery,
+  // fillOutQuestionAnswer,
+  deleteQuestion,
+  addForm,
+  goBack,
+  goNext,
+} = createFormSlice.actions;
+export default createFormSlice.reducer;
