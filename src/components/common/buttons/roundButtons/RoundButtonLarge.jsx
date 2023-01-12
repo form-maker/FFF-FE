5db2faf0bbcff4cdfb9068166be57470619bb203
Suebin An <jejupalette@naver.com>
2023-01-12 14:26:49 +0900

    design : round Button margin, onClick, width, background

--- src/components/common/buttons/roundButtons/RoundButtonLarge.jsx
+++ src/components/common/buttons/roundButtons/RoundButtonLarge.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import fonts from "../../../../styles/fonts";
 
-const RoundButtonLarge = ({ buttonValue, margin, onClick }) => {
+const RoundButtonLarge = ({
+  buttonValue,
+  margin,
+  onClick,
+  width,
+  backgroundColor,
+}) => {
   return (
-    <Button margin={margin} onClick={onClick}>
+    <Button
+      margin={margin}
+      onClick={onClick}
+      width={width}
+      backgroundColor={backgroundColor}
+    >
       {buttonValue}
     </Button>
   );
@@ -12,11 +23,13 @@ const RoundButtonLarge = ({ buttonValue, margin, onClick }) => {
 
 const Button = styled.button`
   padding: 0.4rem 1.5rem;
-  ${fonts.Body2}
+  ${fonts.Body1}
   border: ${({ theme }) => `2px solid ${theme.subColor}`};
+  width: ${({ width }) => width};
   border-radius: 2rem;
   background: transparent;
   margin: ${({ margin }) => margin || "0 1rem"};
+  background-color: ${({ backgroundColor }) => backgroundColor};
   cursor: pointer;
   &:hover {
     background-color: ${({ theme }) => theme.subColor};
