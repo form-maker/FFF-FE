3b6038d3d3c2b3893a1964f274d4a859a6f3746a
Suebin An <jejupalette@naver.com>
2023-01-12 14:41:52 +0900

    feat : createForm 페이지 구현

--- src/pages/CreateForm.jsx
+++ src/pages/CreateForm.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import CreateFromMainScreen from "../components/createForm/createFormMain/screen/CreateFromMainScreen";
+import CreateFormPreviewScreen from "../components/createForm/createFormPreview/screen/CreateFormPreviewScreen";
+import CreateFormSideScreen from "../components/createForm/createFormSide/screen/CreateFormSideScreen";
 
 const CreateForm = () => {
-  return <div>hi</div>;
+  return (
+    <Container>
+      <CreateFormSideScreen />
+      <CreateFromMainScreen />
+      <CreateFormPreviewScreen />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+`;
 
 export default CreateForm;
