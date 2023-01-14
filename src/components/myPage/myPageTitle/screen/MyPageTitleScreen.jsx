1577f20fbbac4062f0a2e15d610bee0ea91a4e9f
Suebin An <jejupalette@naver.com>
2023-01-14 10:57:39 +0900

    design : 마이페이지 타이틀 디자인

---
+++ src/components/myPage/myPageTitle/screen/MyPageTitleScreen.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import MyPageTitle from "../components/MyPageTitle";
+
+const MyPageTitleScreen = () => {
+  return (
+    <Container>
+      <MyPageTitle />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  padding-top: 8.9rem;
+  padding-left: 3rem;
+`;
+
+export default MyPageTitleScreen;
