c51d00431b9d4646e85fdda9557adbfabb3f77d0
Suebin An <jejupalette@naver.com>
2023-01-16 09:16:08 +0900

    design : global 폰트, 색상 명 수정

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -4,50 +4,25 @@ import { css } from "styled-components";
 export default {
   H1: css`
     font-family: "netmarbleB";
-    font-size: 28px;
-    line-height: 38px;
-    letter-spacing: -0.01em;
   `,
 
   H2: css`
-    font-family: "netmarbleB";
-    font-size: 24px;
-    line-height: 34px;
-    letter-spacing: -0.01em;
+    font-family: "netmarbleM";
   `,
 
   H3: css`
-    font-family: "netmarbleB";
-    font-size: 20px;
-    line-height: 26px;
-    letter-spacing: -0.01em;
-  `,
-
-  H4: css`
-    font-family: "netmarbleB";
-    font-size: 18px;
-    line-height: 24px;
-    letter-spacing: -0.01em;
+    font-family: "netmarbleL";
   `,
   Body1: css`
-    font-family: "Inter", sans-serif;
-    font-weight: 800;
-    font-size: 16px;
-    line-height: 20px;
-    letter-spacing: -0.01em;
+    font-family: "Pretendard";
+    color: #000000;
   `,
   Body2: css`
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 18px;
-    letter-spacing: -0.01em;
+    font-family: "Pretendard";
+    color: #5e5e5e;
   `,
-  Caption: css`
-    font-family: "Inter", sans-serif;
-    font-weight: 200;
-    font-size: 12px;
-    line-height: 14px;
-    letter-spacing: -0.01em;
+  Body2: css`
+    font-family: "Pretendard";
+    color: #686868;
   `,
 };
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,8 @@
 export const darkTheme = {
   color: "white",
   backgroundColor: "rgb(50,50,50)",
-  mainColor: "rgb(220,220,220)",
-  subColor: "#A0D5FA",
+  mainColor: "#B0D1FF",
+  subColor: "#BBE0FA",
   sideColor1: "#D2E3FC",
   sideColor2: "#EEEFF2",
   pointColor1: "#FCE0BC",
@@ -13,12 +13,14 @@ export const darkTheme = {
 export const lightTheme = {
   color: "#000000",
   backgroundColor: "#FFFFFF",
-  mainColor: "#BBE0FA",
-  subColor: "#7BBEED",
-  sideColor1: "#D2E3FC",
-  sideColor2: "#EEEFF2",
-  pointColor1: "#FCE0BC",
-  pointColor2: "#DFD9FA",
-  fontColor: "#686868",
-  hoverColor: "#8CB7D7",
+
+  mainColor: "#B0D1FF",
+  subColor1: "#BBE0FA",
+  subColor2: "#CEDFFF",
+  subColor3: "#F6EAFD",
+
+  mainHoverColor: "#94B6E7",
+  subHoverColor1: "#7BBEED",
+  subHoverColor2: "#ADC2EB",
+  subHoverColor3: "#E7D1F3",
 };
