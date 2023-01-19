b527c752aaf2c67e0cf526b7cc979b92668c0fb5
Suebin An <jejupalette@naver.com>
2023-01-19 12:26:34 +0900

    feat : 폰트 추가

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -29,4 +29,12 @@ export default {
     font-family: "Pretendard";
     color: #6c6464;
   `,
+  Body5: css`
+    font-family: "Pretendard";
+    color: #9e9e9e;
+  `,
+  Body6: css`
+    font-family: "Pretendard";
+    color: #999999;
+  `,
 };
