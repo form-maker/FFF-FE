d23222af197f175186d9b9024d0ba09ffae7153b
Suebin An <jejupalette@naver.com>
2023-01-20 15:56:07 +0900

    feat : 날짜 제한 기능 추가

--- src/components/stats/statsMainList/components/Calender.jsx
+++ src/components/stats/statsMainList/components/Calender.jsx
@@ -4,13 +4,17 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Calender.module.css";
 import ko from "date-fns/esm/locale/ko/index.js";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { __getStats } from "../../../../redux/modules/statsSlice";
 import { useParams } from "react-router-dom";
 
 const Calender = ({ setIsToggleOn }) => {
   const { surveyId } = useParams();
   const dispatch = useDispatch();
+  const statsList = useSelector((state) => state.stats.stats);
+
+  let minDate = new Date(statsList?.startedAt);
+  let maxDate = new Date(statsList?.endedAt);
 
   // 달력 날짜 변경시 기준점이 되는 날짜
   const [startDate, setStartDate] = useState(new Date());
@@ -42,6 +46,8 @@ const Calender = ({ setIsToggleOn }) => {
       onChange={onChange}
       startDate={startDate}
       endDate={endDate}
+      minDate={minDate}
+      maxDate={maxDate}
       selectsRange
       inline
       locale={ko}
