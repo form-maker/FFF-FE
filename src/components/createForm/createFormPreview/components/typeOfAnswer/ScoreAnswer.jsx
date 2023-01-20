01d3b74694821c005b13ec085c680699957707d2
Suebin An <jejupalette@naver.com>
2023-01-20 17:39:35 +0900

    refactor : preview answer 위치 수정

--- src/components/createForm/createFormPreview/components/typeOfAnswer/ScoreAnswer.jsx
+++ src/components/createForm/createFormPreview/components/typeOfAnswer/ScoreAnswer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import fonts from "../../../styles/fonts";
-import RoundButtonMedium from "../buttons/roundButtons/RoundButtonMedium";
+import fonts from "../../../../../styles/fonts";
+import RoundButtonMedium from "../../../../common/buttons/roundButtons/RoundButtonMedium";
 
 const ScoreAnswer = () => {
   return (
