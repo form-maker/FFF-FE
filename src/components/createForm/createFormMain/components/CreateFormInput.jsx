a0389a8ef8c596255244edaf0b8162b78ebf321d
Suebin An <jejupalette@naver.com>
2023-01-24 15:33:28 +0900

    fix : 옵셔널 체이닝 추가

--- src/components/createForm/createFormMain/components/CreateFormInput.jsx
+++ src/components/createForm/createFormMain/components/CreateFormInput.jsx
@@ -17,11 +17,14 @@ const CreateFormInput = () => {
     (state) => state.createForm?.selectedFormType
   );
 
+  console.log(questionType);
+
   const questionTitle = useSelector(
     (state) =>
-      state.createForm.formList?.questionList?.length !== 0 &&
+      state.createForm?.formList?.questionList?.length !== 0 &&
       questionType !== "COVER" &&
       selectedFormType !== "COVER" &&
+      questionType !== "NEW_FORM" &&
       state.createForm?.formList?.questionList[currentPageNum - 2][
         "questionTitle"
       ]
@@ -32,6 +35,7 @@ const CreateFormInput = () => {
       state.createForm.formList?.questionList.length !== 0 &&
       questionType !== "COVER" &&
       selectedFormType !== "COVER" &&
+      questionType !== "NEW_FORM" &&
       state.createForm.formList?.questionList[currentPageNum - 2][
         "questionSummary"
       ]
