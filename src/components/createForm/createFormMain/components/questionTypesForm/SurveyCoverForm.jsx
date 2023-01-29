9cc016d3e9adcec0e497696cb318b900f99484de
Suebin An <jejupalette@naver.com>
2023-01-30 05:56:53 +0900

    remove : 필요없는 내용 삭제

--- src/components/createForm/createFormMain/components/questionTypesForm/SurveyCoverForm.jsx
+++ src/components/createForm/createFormMain/components/questionTypesForm/SurveyCoverForm.jsx
@@ -4,33 +4,11 @@ import styled from "styled-components";
 
 import { changeField } from "../../../../../redux/modules/createFormSlice";
 import fonts from "../../../../../styles/fonts";
-import CreateFormCalender from "../calender/CreateFormCalender";
 
 const SurveyCoverForm = () => {
   const dispatch = useDispatch();
-  const wrapperRef = useRef();
   const title = useSelector((state) => state.createForm.formList?.title);
   const summary = useSelector((state) => state.createForm.formList?.summary);
-  const achievement = useSelector(
-    (state) => state.createForm.formList?.achievement
-  );
-  const [isStartDateToggleOpen, setIsStartDateToggleOpen] = useState(false);
-
-  useEffect(() => {
-    const clickOutSide = (event) => {
-      if (
-        isStartDateToggleOpen &&
-        wrapperRef.current &&
-        !wrapperRef.current?.contains(event.target)
-      ) {
-        setIsStartDateToggleOpen(false);
-      }
-    };
-    document.addEventListener("mousedown", clickOutSide);
-    return () => {
-      document.removeEventListener("mousedown", clickOutSide);
-    };
-  }, [isStartDateToggleOpen]);
 
   const InputHandler = (event) => {
     const { name, value } = event.target;
@@ -43,24 +21,6 @@ const SurveyCoverForm = () => {
     );
   };
 
-  const InputIncDecHandler = (num) => {
-    if (achievement < 1 && num < 0) {
-      alert("1보다 크게 설정해주세요");
-    } else {
-      dispatch(
-        changeField({
-          form: "formList",
-          key: "achievement",
-          value: +achievement + +num,
-        })
-      );
-    }
-  };
-
-  const startDateToggleHandler = () => {
-    setIsStartDateToggleOpen((prev) => !prev);
-  };
-
   return (
     <Container>
       <Header>
@@ -70,6 +30,10 @@ const SurveyCoverForm = () => {
           name="title"
           onChange={InputHandler}
         ></TitleInput>
+        {title?.length > 24 && (
+          <p>24자 이내로 줄여주세요 (현 {title?.length}자)</p>
+        )}
+        {title?.length <= 24 && <p>{title?.length}자 작성</p>}
       </Header>
       <textarea
         type="text"
@@ -79,46 +43,10 @@ const SurveyCoverForm = () => {
         resize="none"
         onChange={InputHandler}
       />
-      {/* <AchievementContainer>
-        <label>목표 인원을 설정해주세요</label>
-        <NumberInputContainer>
-          <button
-            onClick={() => {
-              InputIncDecHandler(-10);
-            }}
-          >
-            -
-          </button>
-          <input
-            type="number"
-            min="1"
-            max="3000"
-            step="10"
-            name="achievement"
-            onChange={InputHandler}
-            value={achievement}
-          />
-          <button
-            onClick={() => {
-              InputIncDecHandler(10);
-            }}
-          >
-            +
-          </button>
-        </NumberInputContainer>
-      </AchievementContainer>
-      <CalenderSelectorContainer>
-        <label onClick={startDateToggleHandler}>
-          설문 기간을 설정해주세요 ▾
-        </label>
-        <CalenderContainer ref={wrapperRef}>
-          {isStartDateToggleOpen && (
-            <CreateFormCalender
-              startDateToggleHandler={startDateToggleHandler}
-            />
-          )}
-        </CalenderContainer>
-      </CalenderSelectorContainer> */}
+      {summary?.length > 100 && (
+        <p>100자 이내로 줄여주세요 (현 {summary?.length}자)</p>
+      )}
+      {summary?.length <= 100 && <p>{summary?.length}자 작성</p>}
     </Container>
   );
 };
@@ -130,10 +58,11 @@ const Container = styled.div`
   width: 100%;
   textarea {
     margin-top: 4.9rem;
+    padding: 1rem;
     width: 50rem;
     min-height: 13rem;
 
-    ${fonts.Body3}
+    ${fonts.Body1}
     font-weight: 500;
     font-size: 1.6rem;
     line-height: 1.9rem;
@@ -148,7 +77,14 @@ const Container = styled.div`
 
     &::placeholder {
       ${fonts.Body3}
+      color:${({ theme }) => theme.gray8}
     }
+  }
+  p {
+    font-weight: 700;
+    font-size: 1.4rem;
+
+    color: ${({ theme }) => theme.pointColor};
   }
 `;
 
@@ -172,71 +108,7 @@ const TitleInput = styled.input`
   border: none;
   border-bottom: ${({ theme }) => `0.2rem solid ${theme.gray3}`};
   &::placeholder {
-    color: ${({ theme }) => theme.color};
-  }
-`;
-
-const CalenderSelectorContainer = styled.div`
-  position: relative;
-  margin-top: 1rem;
-  label {
-    ${fonts.Body1}
-    font-weight: 500;
-    font-size: 1.6rem;
-    line-height: 1.9rem;
-
-    border-bottom: ${({ theme }) => `0.2rem solid ${theme.gray3}`};
-    cursor: pointer;
-  }
-`;
-
-const CalenderContainer = styled.div`
-  position: absolute;
-  top: 3rem;
-  left: -3rem;
-`;
-
-const AchievementContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 3rem;
-  label {
-    ${fonts.Body1}
-    font-weight: 500;
-    font-size: 1.6rem;
-    line-height: 1.9rem;
-
-    border-bottom: ${({ theme }) => `0.2rem solid ${theme.gray3}`};
-    cursor: pointer;
-  }
-`;
-
-const NumberInputContainer = styled.div`
-  display: flex;
-  align-items: center;
-
-  margin-top: 1rem;
-  button {
-    margin: 1rem;
-    width: 4rem;
-    height: 4rem;
-
-    ${fonts.Body2}
-    font-size: 3rem;
-    font-weight: 100;
-
-    background: ${({ theme }) => theme.mainColor};
-    border: none;
-    border-radius: 50%;
-  }
-  input {
-    padding: 1rem;
-    width: 5rem;
-
-    text-align: center;
-    border-radius: 0.5rem;
-    border: ${({ theme }) => `0.2rem solid ${theme.gray3}`};
+    color: ${({ theme }) => theme.gray8};
   }
 `;
 
