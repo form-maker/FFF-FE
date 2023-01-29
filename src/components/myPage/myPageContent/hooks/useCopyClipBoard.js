f5cb0a50686dcb9558e20802396c04c2115b62d3
Suebin An <jejupalette@naver.com>
2023-01-29 16:35:44 +0900

    feat : 설문조사 링크 마이페이지 복사 기능 구현

---
+++ src/components/myPage/myPageContent/hooks/useCopyClipBoard.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const useCopyClipBoard = () => {
+  const [isCopy, setIsCopy] = useState(false);
+
+  const onCopy = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setIsCopy(true);
+      alert(`설문조사 링크 복사완료`);
+      return true;
+    } catch (error) {
+      console.error(error);
+      setIsCopy(false);
+
+      return false;
+    }
+  };
+
+  return [isCopy, onCopy];
+};
+
+export default useCopyClipBoard;
