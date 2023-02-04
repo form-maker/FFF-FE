cda601bc0c5b9004b10d394354c7174b31ba5205
Suebin An <jejupalette@naver.com>
2023-02-04 09:06:04 +0900

    remove : 불필요한 주석제거

--- src/components/createForm/createFormPreview/components/typeOfAnswer/CoverAnswer.jsx
+++ src/components/createForm/createFormPreview/components/typeOfAnswer/CoverAnswer.jsx
@@ -9,9 +9,6 @@ const CoverAnswer = () => {
   const title = useSelector((state) => state.createForm.formList.title);
   const summary = useSelector((state) => state.createForm.formList.summary);
 
-  console.log(summary);
-  console.log(summary.replace(/(?:\r\n|\r|\n)/g, "<br />"));
-
   return (
     <Container>
       <Header>
