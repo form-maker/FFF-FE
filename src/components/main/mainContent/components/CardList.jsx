5bf6ad2c810e97faf4056adf25f0f6fa2de27007
Suebin An <jejupalette@naver.com>
2023-02-04 09:06:46 +0900

    fix : 처음에 두번 무한 스크롤 처리되는 부분 문제 수정

--- src/components/main/mainContent/components/CardList.jsx
+++ src/components/main/mainContent/components/CardList.jsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import MainSurveySummeryCard from "./MainSurveySummeryCard";
 import { useNavigate } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
+import fonts from "../../../../styles/fonts";
 
 const CardList = () => {
   const dispatch = useDispatch();
@@ -21,7 +22,6 @@ const CardList = () => {
     if (mainCardList?.length === 0) {
       console.log("첫 포스팅 로딩");
       dispatch(__getMainCardList({ page: 1, size: 9, sortBy: "최신순" }));
-      return;
     }
   }, [dispatch, mainCardList?.length]);
 
@@ -51,6 +51,8 @@ const CardList = () => {
 
   return (
     <Container>
+      {mainCardList?.length === 0 && <h3>현재 진행중인 폼이 없습니다</h3>}
+
       <SurveyContainer>
         {mainCardList?.map((card) => {
           return (
@@ -79,8 +81,16 @@ const CardList = () => {
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+
   width: 100%;
+  h3 {
+    margin: 10rem;
+    ${fonts.Body6}
+    font-size: 3rem;
+  }
 `;
 
 const SurveyContainer = styled.div`
@@ -97,7 +107,7 @@ const SurveyContainer = styled.div`
   @media screen and (max-width: 500px) {
     grid-row-gap: 1rem;
     grid-column-gap: 1rem;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(1, 1fr);
   }
 `;
 
