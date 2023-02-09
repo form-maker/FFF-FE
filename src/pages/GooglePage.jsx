4c29f6779078aec0ed5381d481cf1055b5356fc0
newrkem <newrkem@gmail.com>
2023-02-09 18:54:18 +0900

    rename: 오타수정

--- src/pages/GooglePage.jsx
+++ src/pages/GooglePage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import GoogleLoginr from "../components/login/outh/GoogleLogin";
+import GoogleLogin from "../components/login/outh/GoogleLogin";
 
 const gpage = () => {
   return (
     <div>
-      <GoogleLoginr />
+      <GoogleLogin />
       <h4>로그인 중...</h4>
     </div>
   );
